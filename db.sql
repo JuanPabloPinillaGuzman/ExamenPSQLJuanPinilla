e64eeb3831da27ac5a2a0059f5743667f54148f2
JuanPabloPinillaGuzman <180667131+JuanPabloPinillaGuzman@users.noreply.github.com>
2025-04-11 09:52:51 -0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -4,7 +4,7 @@ CREATE DATABASE tech_zone;
 \c tech_zone
 
 CREATE TABLE proveedores(
-	id_proveedores SERIAL PRIMARY KEY,
+    id_proveedores SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     correo VARCHAR(50) NOT NULL,
     telefono VARCHAR(50)
@@ -12,22 +12,23 @@ CREATE TABLE proveedores(
 
 CREATE TABLE categorias(
     id_categorias SERIAL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
+    nombre VARCHAR(50) NOT NULL
 );
 
+
 CREATE TABLE productos(
-	id_productos SERIAL PRIMARY KEY,
-	nombre VARCHAR(50) NOT NULL,
-    precio NUMERIC(10,2)NOT NULL CHECK(precio>=0),
-    stock NOT NULL CHECK(stock>=0),
+    id_productos SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    precio NUMERIC(10,2) NOT NULL CHECK(precio>=0),
+    stock INTEGER NOT NULL CHECK(stock>=0),
     id_proveedores INTEGER NOT NULL,
-	id_categorias INEGER NOT NULL,
+    id_categorias INTEGER NOT NULL,
     FOREIGN KEY (id_categorias) REFERENCES categorias(id_categorias),
-    FOREIGN KEY (id_proveedores) REFERENCES proveedores(id_proveedores),
+    FOREIGN KEY (id_proveedores) REFERENCES proveedores(id_proveedores)
 );
 
 CREATE TABLE clientes(
-	id_clientes SERIAL PRIMARY KEY,
+    id_clientes SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     correo VARCHAR(50) NOT NULL,
     telefono VARCHAR(50) NOT NULL
@@ -37,15 +38,15 @@ CREATE TABLE ventas(
     id_ventas SERIAL PRIMARY KEY,
     fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     id_clientes INTEGER NOT NULL,
-    FOREIGN KEY id_clientes REFERENCES clientes(id_clientes)
+    FOREIGN KEY (id_clientes) REFERENCES clientes(id_clientes)
 );
 
 CREATE TABLE registro_venta (
-	id_registo SERIAL PRIMARY KEY,
+    id_registo SERIAL PRIMARY KEY,
     cantidad INTEGER NOT NULL CHECK (cantidad > 0),
     subtotal NUMERIC(10,2) NOT NULL CHECK (subtotal > 0),
     id_ventas INTEGER NOT NULL,
     id_productos INTEGER NOT NULL,
-    FOREIGN KEY id_ventas REFERENCES ventas(id_ventas),
-	FOREIGN KEY id_productos REFERENCES productos(id_productos),
+    FOREIGN KEY (id_ventas) REFERENCES ventas(id_ventas),
+    FOREIGN KEY (id_productos) REFERENCES productos(id_productos)
 );
